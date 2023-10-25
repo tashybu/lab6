9d40834c57dc1ddf2c995659c674b7518d509e0c
tashybu <142923555+tashybu@users.noreply.github.com>
2023-10-25 17:09:46 -0400

    Add files via upload

---
+++ lab6.py
@@ -0,0 +1,36 @@
+# Natasha Buehler
+
+def encode(password):
+    # Encodes input by increasing each digit by 3
+    result = ""
+    for i in range(0, len(password)):
+        result += str(int(password[i]) + 3)
+    return result
+
+
+def menu():
+    print("Menu\n"
+          "-------------\n"
+          "1. Encode\n"
+          "2. Decode\n"
+          "3. Quit")
+
+
+def main():
+    user_choice = 0
+    password = ""
+    encoded_pass = ""
+
+    while user_choice != 3:
+        menu()
+        user_choice = int(input("\nPlease enter an option: "))
+
+        if user_choice == 1:
+            password = input("Please enter your password to encode: ")
+            encoded_pass = encode(password)
+            print("Your password has been encoded and stored!")
+        ''' uncomment after adding decode method
+        elif user_choice == 2:
+            print("The encoded password is " + encoded_pass + ", and the original password is " + decode(encoded_pass) + ".")
+        '''
+main()
